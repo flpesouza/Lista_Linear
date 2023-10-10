6f17f67d75abc19a254339020f7816916c677eac
Felipe Souza <67338813+flpesouza@users.noreply.github.com>
2023-10-10 15:02:39 -0300

    Add files via upload

---
+++ Lista.cs
@@ -0,0 +1,163 @@
+﻿using System;
+class Lista
+{
+    private int[] array;
+    private int n;
+    /**
+    * Construtor da classe.
+*/
+    public Lista(int tamanho)
+    {
+        array = new int[tamanho];
+        n = 0;
+    }
+    /**
+    * Insere um elemento na primeira posicao da lista e move os demais
+    * elementos para o fim da lista.
+*/
+    public void InserirInicio(int x)
+    {
+        if (n >= array.Length)
+        {
+            throw new Exception("Erro ao inserir!");
+        }
+        for (int i = n; i > 0; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[0] = x;
+        n++;
+    }
+    /**
+    * Insere um elemento na ultima posicao da lista.
+*/
+    public void InserirFim(int x)
+    {
+        if (n >= array.Length)
+        {
+            throw new Exception("Erro ao inserir!");
+        }
+        array[n] = x;
+        n++;
+    }
+    /**
+    * Insere um elemento em uma posicao especifica e move os demais
+    * elementos para o fim da lista.
+*/
+    public void Inserir(int x, int pos)
+    {
+        if (n >= array.Length || pos < 0 || pos > n)
+        {
+            throw new Exception("Erro ao inserir!");
+        }
+        for (int i = n; i > pos; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[pos] = x;
+        n++;
+    }
+    /**
+    * Remove um elemento da primeira posicao da lista e movimenta
+    * os demais elementos para o inicio da mesma.
+*/
+    public int RemoverInicio()
+    {
+        if (n == 0)
+        {
+            throw new Exception("Erro ao remover!");
+        }
+        int resp = array[0];
+        n--;
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = array[i + 1];
+        }
+        return resp;
+    }
+    /**
+    * Remove um elemento da ultima posicao da lista.
+*/
+    public int RemoverFim()
+    {
+        if (n == 0)
+        {
+            throw new Exception("Erro ao remover!");
+        }
+        n--;
+        return array[n];
+    }
+
+    public int RemoverItem(int x)
+    {
+        int resp = 0;
+        int pos = 0;
+        if (n == 0)
+        {
+            throw new Exception("Erro ao remover!");
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if(x == array[i])
+            {
+                resp = x;
+                pos = i;
+                n--;
+            }
+        }
+        for (int i = pos; i < n; i++)
+        {
+            array[i] = array[i + 1];
+        }
+        return resp;
+    }
+
+    public int Contar(int x)
+    {
+        int cont = 0;
+        if (n == 0)
+        {
+            throw new Exception("Erro ao remover!");
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (x == array[i])
+            {
+                cont++;
+            }
+        }
+        return cont;
+    }
+    /**
+    * Remove um elemento de uma posicao especifica da lista e
+    * movimenta os demais elementos para o inicio da mesma.
+*/
+    public int Remover(int pos)
+    {
+        if (n == 0 || pos < 0 || pos >= n)
+        {
+            throw new Exception("Erro ao remover!");
+        }
+        int resp = array[pos];
+        n--;
+        for (int i = pos; i < n; i++)
+        {
+            array[i] = array[i + 1];
+        }
+        return resp;
+    }
+    /**
+    * Mostra os elementos da lista separados por espacos.
+*/
+    public void Mostrar()
+    {
+        Console.Write("[ ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine("]");
+    }
+
+   
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Lixo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o tamanho da sua lista: ");
+            int tamanho = int.Parse(Console.ReadLine());
+            Lista lista = new Lista(tamanho);
+
+            int tempo = 0;
+            int pos = 0;
+            int opcao = 0;
+
+            while (opcao != 12)
+            {
+
+                Console.WriteLine("MENU\n1) Inserir um tempo no início da lista\n2) Inserir um tempo no final da lista\n3) Inserir um tempo numa posição específica da lista\n4) Remover o primeiro tempo da lista(Imprimir o tempo removido)\n5) Remover o último tempo da lista(Imprimir o tempo removido)\n6) Remover um tempo de uma posição específica na lista(O usuário deve informar a posição do tempo a serremovido.Imprimir o tempo removido)\n7) Remover um tempo específico da lista(O usuário deve informar o tempo a ser removido)\n8) Pesquisar quantas vezes um determinado tempo consta na lista(O usuário deve informar o tempo a ser pesquisado)\n9) Mostrar todos os tempos da lista\n10)Encerrar o programa\nDeseje a opção desejada: ");
+                opcao = int.Parse(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Digite o tempo que deseja inserir no inicio da fila: ");
+
+                        tempo = int.Parse(Console.ReadLine());
+
+                        lista.InserirInicio(tempo);
+                        break;
+                    case 2:
+                        Console.WriteLine("Digite o tempo que deseja inserir no fim da fila: ");
+
+                        tempo = int.Parse(Console.ReadLine());
+                        lista.InserirFim(tempo);
+                        break;
+                    case 3:
+                        Console.WriteLine("Digite o tempo que deseja inserir: ");
+
+                        tempo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite a posição que deseja inseri - lo: ");
+
+                        pos = int.Parse(Console.ReadLine());
+                        lista.Inserir(pos, tempo);
+                        break;
+                    case 4:
+                        Console.WriteLine("Primeiro tempo removido: " + lista.RemoverInicio());
+                        break;
+                    case 5:
+                        Console.WriteLine("Ultimo tempo removido: " + lista.RemoverFim());
+                        break;
+                    case 6:
+                        Console.WriteLine("Digite a posição da qual deseja remover o tempo: ");
+
+                        pos = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Tempo removido: " + lista.Remover(pos));
+                        break;
+                    case 7:
+                        Console.WriteLine("Digite o tempo que deseja remover: ");
+
+                        tempo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Item removido: " + lista.RemoverItem(tempo));
+                        break;
+                    case 8:
+                        Console.WriteLine("Digite o tempo que deseja pesquisar: ");
+
+                        tempo = int.Parse(Console.ReadLine());
+                        Console.WriteLine("O tempo foi registrado " + lista.Contar(tempo) + " vezes");
+                        break;
+                    case 9:
+                        lista.Mostrar();
+                        break;
+                    case 10:
+                        Console.WriteLine("FIM");
+                        break;
+                    default:
+                        Console.WriteLine("Número invalido!");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
